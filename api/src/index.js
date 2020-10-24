444a97b6b9b878fc120236ca2c861358fe110f0a
Jordan Cooper <jordan@cooperplanet.com>
2020-10-24 14:21:30 -0700

    Make api reload on changes

--- api/src/index.js
+++ api/src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3002
 
-app.get('/', (req, res) => res.send('Hello world!'))
+app.get('/', (req, res) => res.send('Hello my good friends!'))
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
 
