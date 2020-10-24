2947aff90ed8de874ac4579842e0b86835714b75
Jordan Cooper <jordan@cooperplanet.com>
2020-10-24 14:17:35 -0700

    Finish docker-compose and set up api

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3002
+
+app.get('/', (req, res) => res.send('Hello world!'))
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
+
+process.on('SIGINT', () => {
+  process.exit()
+})
