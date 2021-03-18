b1df0f989b365547a202968440b0c1034381b2b3
Jordan Cooper <jordan@cooperplanet.com>
2021-03-17 23:00:28 -0700

    Update UI

--- app/src/components/impersonate/impersonate.js
+++ app/src/components/impersonate/impersonate.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './impersonate.scss'
 
 const Impersonate = props => {
+  const [messageText, setMessageText] = useState('')
   const { name, avatarUrl } = props.npcData
 
   return (
@@ -13,7 +14,19 @@ const Impersonate = props => {
         <img src={avatarUrl} className="avatar-image" />
         <span className="npc-name">{name}</span>
       </div>
-      <div className="right-container"></div>
+      <div className="right-container">
+        <div className="message-container">
+          <label className="message-label" htmlFor="npc-message">
+            Message:
+          </label>
+          <textarea
+            id="npc-message"
+            className="npc-message"
+            value={messageText}
+            onChange={e => setMessageText(e.target.value)}
+          />
+        </div>
+      </div>
     </div>
   )
 }
